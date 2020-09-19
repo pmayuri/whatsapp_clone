2893b9420266c6f72628de7e926f6f7eb67d0009
mayuri <pmayuri124@gmail.com>
2020-09-19 18:04:16 +0530

    icons completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import './Sidebar.css';
 
 function Sidebar() {
@@ -7,12 +11,24 @@ function Sidebar() {
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar/>
+            <div className="sidebar__headerRight">
+                <IconButton>
+                    <DonutLargeIcon />
+                 </IconButton>
+                 <IconButton>
+                    <ChatIcon />
+                </IconButton>
+                <IconButton>
+                     <MoreVertIcon/>
+                </IconButton>
+
+                </div>
             </div>
+
             <div className="sidebar__search">
 
             </div>
-
-            <div className="sidebar__chats">
+             <div className="sidebar__chats">
                 
             </div>
 
