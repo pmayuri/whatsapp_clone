5884fd43748a50c3119d95f8c8a4fceca1bc0340
mayuri <pmayuri124@gmail.com>
2020-09-20 23:48:02 +0530

    useeffect

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,16 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React ,{useEffect,useState}from 'react';
 import "./SidebarChat.css";
 
 function SidebarChat() {
+    const [seed,setSeed] = useState("");
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()* 5000));
+    }, []);
     return (
         <div className="sidebarChat">
-        <Avatar src="https://avatars.dicebear.com/api/human/1dskfljlk123.svg"/>
+         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
        <div className="sidebarChat__info">
            <h2>Room name</h2>
            <p> last message...</p>
